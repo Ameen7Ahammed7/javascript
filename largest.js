986fde49511cadf966e8e8f766fac2119cef3507
Ameen7Ahammed7 <109576086+Ameen7Ahammed7@users.noreply.github.com>
2022-08-31 12:52:48 +0530

    Add files via upload

---
+++ largest.js
@@ -0,0 +1,15 @@
+var num1 = 120;
+var num2 =130;
+var num3 =140;
+
+var total = num1+num2+num3
+ 
+//The percent that we want to get.
+//i.e. We want to get 50% of 120.
+var percentToGet = 50;
+ 
+//Calculate the percent.
+
+var percent = ( total/ 100) * percentToGet;
+
+console.log( percent + "%");
